e9a73b27cf4f4a122075e8cf5c0d1e6fd27c99af
Takashi Masuda <masutaka.net@gmail.com>
2015-06-15 01:05:46 +0900

    initial commit

---
+++ clock.rb
@@ -0,0 +1,11 @@
+require 'active_support/time'
+require 'erb'
+require 'yaml'
+
+path = File.join(__dir__, 'settings.yml')
+
+settings = YAML.load(ERB.new(IO.read(path)).result)
+
+require_relative 'lib/awesome-feeds'
+
+AwesomeFeeds.new(settings).start
---
+++ lib/awesome-feeds.rb
@@ -0,0 +1,41 @@
+require 'clockwork'
+require 'logger'
+require 'open-uri'
+require 'rexml/document'
+
+class AwesomeFeeds
+  def initialize(settings)
+    @feed_url = settings['feed_url']
+    @exclusion_keyword = settings['exclusion_keyword']
+    @path_to_output = settings['path_to_output']
+    @element_name = settings['element_name']
+    @fetch_interval_seconds = settings['fetch_interval_seconds']
+    @logger = Logger.new(STDOUT)
+  end
+
+  def start
+    Clockwork::handler {|job| send(job)}
+    Clockwork::every(@fetch_interval_seconds, :create)
+  end
+
+  private
+
+  def create
+    doc = REXML::Document.new(open(@feed_url))
+    @logger.info("fetch #{@feed_url}")
+
+    doc.context[:attribute_quote] = :quote
+    root = doc.root
+
+    root.elements.each do |element|
+      next unless element.name == @element_name
+      next unless element.to_s.include?(@exclusion_keyword)
+      root.delete_element(element)
+    end
+
+    File.open(@path_to_output, 'w') do |outfile|
+      doc.write(outfile)
+      @logger.info("create #{@path_to_output}")
+    end
+  end
+end
