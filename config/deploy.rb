c077435c5ec22172fc37d7a89bc098fb71fbd52c
Takashi Masuda <masutaka.net@gmail.com>
2015-07-12 20:42:49 +0900

    Add recording deployments

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,6 +37,8 @@ set :linked_dirs, %w{contents log tmp vendor/bundle}
 set :bundle_path, -> { shared_path.join('vendor', 'bundle') }
 set :clockwork_file, -> { 'clockwork.rb' }
 
+after 'deploy:updated', 'newrelic:notice_deployment'
+
 namespace :deploy do
   desc 'Get settings.yml'
   before :updated, :setting_file do
