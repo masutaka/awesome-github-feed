00523b28dc6939ead58ae06fcf1b97b5bf732cd8
Takashi Masuda <masutaka.net@gmail.com>
2015-07-12 23:44:11 +0900

    Add cap `ask :branch` except in CircleCi

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'awesome-github-feed'
 set :repo_url, 'git@github.com:masutaka/awesome-github-feed.git'
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp unless ENV['CIRCLECI']
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/var/www/vhosts/github.masutaka.net'
